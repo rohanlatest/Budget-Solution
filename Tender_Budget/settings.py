450804390a273c9737677879ffde855b16f68f04
rohanlatest <rohankumark.latest@gmail.com>
2021-05-07 20:26:53 +0530

    Initial commit

--- Tender_Budget/settings.py
+++ Tender_Budget/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f(72b73mgmpj!#!9i+gk3zaz)rnpkw34@lw!^@+n@e7@17vq9j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
